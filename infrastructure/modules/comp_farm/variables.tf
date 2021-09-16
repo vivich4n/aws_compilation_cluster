6d94fdb6fab52c21f565695460a543aec6add2f4
Ramon Esparza <ramonesparza@pop-os.localdomain>
2021-09-16 16:51:39 -0500

    typo

---
+++ infrastructure/modules/comp_farm/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_id" {}
+
+variable "private_subnets"{}
+
+variable "ins_type"{}
+
+variable "ami"{}
+
+variable "env"{}
+
+variable "ec2number" {}
+
+variable "ec2min" {}
+
+variable "ec2max" {}
+
+variable "ssh_key" {}
---
+++ infrastructure/modules/ec2_main/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {}
+
+variable "public_subnet" {}
+
+variable "ins_type"{}
+
+variable "ami"{}
+
+variable "env"{}
+
+variable "ssh_key"{}
